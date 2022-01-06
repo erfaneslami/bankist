21e29156afddcbc9e2ee568c62047e5e41c4b267
erfan eslami <erfan.88.eslami@gmail.com>
2022-01-06 15:12:01 +0330

    fix logo size in login page

---
+++ script/app.js
@@ -0,0 +1,7 @@
+document
+  .querySelector(".login__box_btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = "../home.html";
+    console.log("clicked");
+  });
