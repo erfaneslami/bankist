f23df73e3a68fc44b083fe605ba7da38fae28b7b
erfan eslami <erfan.88.eslami@gmail.com>
2022-01-29 16:35:59 +0330

    refactor home.js to OOP

--- script/home.js
+++ script/home.js
@@ -5,49 +5,8 @@
 // BANKIST APP
 
 /////////////////////////////////////////////////
-// Data
-// const account1 = {
-//   owner: "Erfan eslami",
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: "erfan123",
-//   cardNumber: `6219 6426 1325 9988`,
-// };
 
-// const account2 = {
-//   owner: "Alireza eslami",
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: "alireza123",
-//   cardNumber: `5024 5644 5236 9956`,
-// };
-
-// const account3 = {
-//   owner: "Nioofar najafi",
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: "niloo123",
-//   cardNumber: `6037 1236 8526 1235`,
-// };
-
-// const account4 = {
-//   owner: "Mohammad Najafi",
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: "mohammad123",
-//   cardNumber: `6219 6512 9845 3215`,
-// };
-
-// const account5 = {
-//   owner: "Ali tehrani",
-//   movements: [300, -250, 390, -1000, -20, 50, 485, -460],
-//   interestRate: 0.7,
-//   pin: "ali123",
-//   cardNumber: `6037 1212 3596 3535`,
-// };
-
-// const accounts = [account1, account2, account3, account4, account5];
-
+/////////////////////////////////////////////////
 const labelBalance = document.querySelector(".card__balance_value");
 const labelOwner = document.querySelector(".card__owner");
 const labelCardNumber = document.querySelector(".card__number");
@@ -61,87 +20,98 @@ const containerMovements = document.querySelector(".movements");
 const containerMovementsInner = document.querySelector(".movements__container");
 
 const btnSort = document.querySelector(".sort");
+/////////////////////////////////////////////////
 
-// ! Display Movements
-const displayMovements = function (acc) {
-  containerMovementsInner.innerHTML = "";
-  acc.movements.forEach((mov, i) => {
-    const type = mov > 0 ? `deposit` : `withdrawal`;
+/////////////////////////////////////////////////
+class App {
+  constructor(account) {
+    this.account = account;
 
-    containerMovementsInner.insertAdjacentHTML(
-      "afterbegin",
-      `
+    this._updateUI(this.account);
+  }
+
+  _updateUI(acc) {
+    this._displayMovements(acc);
+    this._displayCard(acc);
+    this._displayWelcomeName(acc);
+    this._calcDisplaySummery(acc);
+  }
+
+  //  Display Movements
+  _displayMovements(acc) {
+    containerMovementsInner.innerHTML = "";
+    acc.movements.forEach((mov, i) => {
+      const type = mov > 0 ? `deposit` : `withdrawal`;
+
+      containerMovementsInner.insertAdjacentHTML(
+        "afterbegin",
+        `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}"> ${
-        i + 1
-      } - ${type}</div>
+          i + 1
+        } - ${type}</div>
       <div class="movements__value">${mov}</div>
     </div>`
-    );
-  });
-};
+      );
+    });
+  }
 
-// ! DISPLAY CARD
-const displayCard = function (acc) {
-  // * Display Bank Name
-  labelBankName.textContent =
-    acc.cardNumber.slice(0, 4) === `6219`
-      ? `Saman Bank`
-      : acc.cardNumber.slice(0, 4) === `5024`
-      ? `Pasargad Bank`
-      : acc.cardNumber.slice(0, 4) === `6037`
-      ? `Saderat Bank`
-      : `un known Bank`;
+  //  DISPLAY CARD
+  _displayCard(acc) {
+    // * Display Bank Name
+    labelBankName.textContent =
+      acc.cardNumber.slice(0, 4) === `6219`
+        ? `Saman Bank`
+        : acc.cardNumber.slice(0, 4) === `5024`
+        ? `Pasargad Bank`
+        : acc.cardNumber.slice(0, 4) === `6037`
+        ? `Saderat Bank`
+        : `un known Bank`;
 
-  // * Display Owner Name
-  labelOwner.textContent = acc.owner;
+    // * Display Owner Name
+    labelOwner.textContent = acc.owner;
 
-  // * Display Card Number
-  labelCardNumber.textContent = acc.cardNumber;
+    // * Display Card Number
+    labelCardNumber.textContent = acc.cardNumber;
 
-  // * Display Balance
-  labelBalance.textContent =
-    acc.movements.reduce((acc, mov) => {
-      return acc + mov;
-    }, 0) + `$`;
-};
+    // * Display Balance
+    labelBalance.textContent =
+      acc.movements.reduce((acc, mov) => {
+        return acc + mov;
+      }, 0) + `$`;
+  }
 
-// ! Display Welcome name
-const displayWelcomeName = (acc) =>
-  (labelWelcome.textContent = `Hi ${acc.owner.split(" ")[0]} !`);
+  //  Display Welcome name
+  _displayWelcomeName(acc) {
+    labelWelcome.textContent = `Hi ${acc.owner.split(" ")[0]} !`;
+  }
 
-// ! DISPLAY SUMMERY
-const calcDisplaySummery = function (acc) {
-  // * INCOME
-  labelIncome.textContent =
-    acc.movements
-      .filter((mov) => mov > 0)
-      .reduce((acc, deposit) => acc + deposit, 0) + `$`;
-
-  // * OUT COME
-  labelOutcome.textContent =
-    Math.abs(
+  //  DISPLAY SUMMERY
+  _calcDisplaySummery(acc) {
+    //  INCOME
+    labelIncome.textContent =
       acc.movements
-        .filter((mov) => mov < 0)
-        .reduce((acc, deposit) => acc + deposit, 0)
-    ) + `$`;
+        .filter((mov) => mov > 0)
+        .reduce((acc, deposit) => acc + deposit, 0) + `$`;
 
-  // * INTEREST
-  labelInterest.textContent =
-    acc.movements
-      .filter((mov) => mov > 0)
-      .map((deposit) => (deposit * acc.interestRate) / 100)
-      .filter((int) => int > 1)
-      .reduce((acc, int) => acc + int, 0) + `$`;
-};
+    //  OUT COME
+    labelOutcome.textContent =
+      Math.abs(
+        acc.movements
+          .filter((mov) => mov < 0)
+          .reduce((acc, deposit) => acc + deposit, 0)
+      ) + `$`;
 
-const updateUI = function (acc) {
-  displayMovements(acc);
-  displayCard(acc);
-  displayWelcomeName(acc);
-  calcDisplaySummery(acc);
-};
+    //  INTEREST
+    labelInterest.textContent =
+      acc.movements
+        .filter((mov) => mov > 0)
+        .map((deposit) => (deposit * acc.interestRate) / 100)
+        .filter((int) => int > 1)
+        .reduce((acc, int) => acc + int, 0) + `$`;
+  }
+}
+/////////////////////////////////////////////////
 
 const currentAcc = JSON.parse(localStorage.getItem("currentAcc"));
-
-updateUI(currentAcc);
+const init = new App(currentAcc);
