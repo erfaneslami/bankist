a1cec9fc93aae50c13523247648d40fdfdf1b18d
erfan eslami <erfan.88.eslami@gmail.com>
2022-02-06 22:40:35 +0330

    remove old styles and pages and add new ones

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Income", "Expense"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [15000, 3000],
+        backgroundColor: [
+          // "rgba(255, 206, 86, 0.0)",
+          "#ba66f9",
+          // "rgba(255, 206, 86, 0.0)",
+          "#03a9f4",
+        ],
+        borderColor: [
+          // "rgba(255, 206, 86, 0)",
+          "#ba66f9",
+          // "rgba(255, 206, 86, 0)",
+          "#03a9f4",
+        ],
+        borderWidth: 1,
+        spacing: 5,
+        cutout: "80%",
+        borderRadius: 30,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
