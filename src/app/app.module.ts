d80a9bb0072f4381a77cb18972a67bf43c187edf
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-03 14:57:34 +0330

    add login & signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntranceComponent } from './entrance/entrance.component';
+import { LoginComponent } from './entrance/login/login.component';
+import { SignupComponent } from './entrance/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntranceComponent
+    EntranceComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- src/app/entrance/entrance.component.ts
+++ src/app/entrance/entrance.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-entrance',
   templateUrl: './entrance.component.html',
-  styleUrls: ['./entrance.component.scss']
+  styleUrls: ['./entrance.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EntranceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
