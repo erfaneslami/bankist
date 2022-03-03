ab522fdaf7c3a790a77ae84b6dc8a6a8d217e529
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-03 15:11:44 +0330

    add entrance route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntranceComponent } from './entrance/entrance.component';
+import { LoginComponent } from './entrance/login/login.component';
+import { SignupComponent } from './entrance/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EntranceComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
