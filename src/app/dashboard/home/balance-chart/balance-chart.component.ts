40181d3d5f3cf2c9725f003cfd1dbc1a0fe5c674
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-06 19:43:45 +0330

    add chart js

--- src/app/dashboard/home/balance-chart/balance-chart.component.ts
+++ src/app/dashboard/home/balance-chart/balance-chart.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-balance-chart',
   templateUrl: './balance-chart.component.html',
-  styleUrls: ['./balance-chart.component.scss']
+  styleUrls: ['./balance-chart.component.scss'],
 })
 export class BalanceChartComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('myChart', { static: true }) myChart: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.creatChart(1000, 17500);
   }
 
+  creatChart(income: number, expense: number) {
+    Chart.register(...registerables);
+    const ctx = this.myChart.nativeElement;
+
+    this.myChart.nativeElement = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['Income', 'Expense'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [income, expense],
+            backgroundColor: ['#ba66f9', '#03a9f4'],
+            borderColor: ['#ba66f9', '#03a9f4'],
+            borderWidth: 1,
+            spacing: 5,
+            borderRadius: 30,
+          },
+        ],
+      },
+      options: {
+        cutout: '80%',
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+  }
 }
