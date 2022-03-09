f404b3acbfa01902881813f349f214fc5fdc2df3
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-09 11:10:43 +0330

    add signup form reactive

--- src/app/entrance/signup/signup.component.ts
+++ src/app/entrance/signup/signup.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      fullName: new FormControl(null),
+      email: new FormControl(null),
+      password: new FormControl(null),
+    });
   }
 
+  onSignup() {
+    console.log(this.signupForm);
+  }
 }
