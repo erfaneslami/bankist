8b27d066141bb4d0651e3a98a1e8e9ff617e58d0
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-12 22:15:50 +0330

    render transactions

--- src/app/dashboard/home/transactions/transactions.component.ts
+++ src/app/dashboard/home/transactions/transactions.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/entrance/auth.service';
+import { User } from 'src/app/entrance/models/user.model';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+  styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.user.subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+    });
   }
-
 }
