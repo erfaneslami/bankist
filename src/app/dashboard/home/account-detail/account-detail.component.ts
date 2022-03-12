23b770def961a7e83b914c4c700b6fba8142e998
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-12 22:09:10 +0330

    render card detail

--- src/app/dashboard/home/account-detail/account-detail.component.ts
+++ src/app/dashboard/home/account-detail/account-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/entrance/auth.service';
+import { User } from 'src/app/entrance/models/user.model';
 
 @Component({
   selector: 'app-account-detail',
   templateUrl: './account-detail.component.html',
-  styleUrls: ['./account-detail.component.scss']
+  styleUrls: ['./account-detail.component.scss'],
 })
 export class AccountDetailComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.user.subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+    });
   }
-
 }
--- src/app/dashboard/home/card/card.component.ts
+++ src/app/dashboard/home/card/card.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/entrance/auth.service';
+import { User } from 'src/app/entrance/models/user.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.user.subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+    });
   }
-
 }
