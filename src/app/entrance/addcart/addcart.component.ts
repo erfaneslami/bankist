9dc2f51580fe27ff942eae05b2df8a3526a4e2b5
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-12 13:41:09 +0330

    add card form

--- src/app/entrance/addcart/addcart.component.ts
+++ src/app/entrance/addcart/addcart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addcart',
   templateUrl: './addcart.component.html',
-  styleUrls: ['./addcart.component.scss']
+  styleUrls: ['./addcart.component.scss'],
 })
 export class AddcartComponent implements OnInit {
-
-  constructor() { }
+  cardForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.cardForm = new FormGroup({
+      ownerName: new FormControl(null, [Validators.required]),
+      cardNumber: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(12),
+        Validators.maxLength(12),
+      ]),
+      cvv2: new FormControl(null, Validators.required),
+      exp: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[0-9][0-9]/[0-9][0-9]'),
+      ]),
+    });
   }
 
+  onSubmit() {}
 }
---
+++ src/app/entrance/models/card.model.ts
@@ -0,0 +1,8 @@
+export class Card {
+  constructor(
+    public ownerName: string,
+    public cardNumber: number,
+    public cvv2: number,
+    public exp: string
+  ) {}
+}
