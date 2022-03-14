49118c5024ab975bab136e32f8f8ade96f4f5343
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-14 21:14:15 +0330

    add transfer reactive form

--- src/app/dashboard/transfer/transfer.component.ts
+++ src/app/dashboard/transfer/transfer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.scss']
+  styleUrls: ['./transfer.component.scss'],
 })
 export class TransferComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  transferForm: FormGroup;
 
   ngOnInit(): void {
+    this.transferForm = new FormGroup({
+      receiverCard: new FormControl(null),
+      transferAmount: new FormControl(null),
+    });
   }
 
+  onTransfer() {
+    if (!this.transferForm.valid) {
+      this.transferForm.markAllAsTouched();
+      return;
+    }
+  }
 }
