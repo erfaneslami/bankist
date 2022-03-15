6e693c0b26bde71cb93595412db5615305bc2245
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-15 13:50:02 +0330

    add loading spinner

--- src/app/dashboard/transfer/transfer.component.ts
+++ src/app/dashboard/transfer/transfer.component.ts
@@ -10,6 +10,7 @@ import { UserService } from 'src/app/entrance/user.service';
 export class TransferComponent implements OnInit {
   constructor(private userService: UserService) {}
   transferForm: FormGroup;
+  isLoading = false;
 
   ngOnInit(): void {
     this.transferForm = new FormGroup({
@@ -20,16 +21,21 @@ export class TransferComponent implements OnInit {
   }
 
   onTransfer() {
+    this.isLoading = true;
     const form = this.transferForm.value;
     if (!this.transferForm.valid) {
       this.transferForm.markAllAsTouched();
+      this.isLoading = false;
       return;
     }
     this.userService
       .transfer(form.receiverCard, form.transferAmount, form.transferDesc)
       .subscribe({
-        next: (res) => {},
+        next: (res) => {
+          this.isLoading = false;
+        },
         error: (error) => {
+          this.isLoading = false;
           console.log(error);
         },
       });
